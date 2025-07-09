ada98bbe8cd65b260f0626f307e8f5c3c8de1510
PopelinovDanilo <popelinov.danilo@gmail.com>
2025-07-09 13:27:25 +0300

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,30 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const costs = 40;
+  let rentalCost = 0;
+
+  if (days <= 0) {
+    return 0;
+  }
+
+  if (days > 0 && days < 3) {
+    rentalCost = costs * days;
+
+    return rentalCost;
+  }
+
+  if (days >= 3 && days < 7) {
+    rentalCost = costs * days - 20;
+
+    return rentalCost;
+  }
+
+  if (days >= 7) {
+    rentalCost = costs * days - 50;
+
+    return rentalCost;
+  }
 }
 
 module.exports = calculateRentalCost;
